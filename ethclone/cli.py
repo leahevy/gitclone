86bd1bb15f5f473ce5174dd24e61a92be3e9937e
Leah Lackner <leah.lackner+github@gmail.com>
2022-04-21 23:01:12 +0200

    Add ethclone package

---
+++ ethclone/cli.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import sys
+
+from ethclone.gitcmds import clone, GitCloneException, CloneProcess
+
+import yaml
+import re
+import pathlib
+
+from github import Github
+
+
+def clone_repo(repostr: str):
+    result = re.search(r"([^\s]+)\s+as\s+(.+)", repostr)
+    if result:
+        url = result.group(1)
+        dest = result.group(2)
+        dest_path = pathlib.Path(dest)
+        parent_dir = dest_path.parents[0]
+        parent_dir.mkdir(parents=True, exist_ok=True)
+
+        if not dest_path.exists():
+            try:
+                print(f"Cloning {url} to {dest}")
+                clone([CloneProcess(url=url, dest=dest)])
+                print()
+            except KeyboardInterrupt:
+                sys.exit(5)
+            except GitCloneException as e:
+                print(str(e))
+                sys.exit(6)
+        else:
+            print(f"Repository {dest} exists")
+
+    else:
+        raise ValueError(repostr)
+
+
+def handle_autofetch(y):
+    repos = []
+    for k, v in y.items():
+        if k == "github.com":
+            if "token" in v:
+                g = Github(v["token"])
+                user = g.get_user()
+            else:
+                g = Github()
+                user = g.get_user(v["user"])
+            for repo in user.get_repos(
+                visibility="all" if v["private-repos"] else "public"
+            ):
+                path = v["path"]
+                path = path.replace("{user}", v["user"])
+                path = path.replace("{repo}", repo.name)
+                if v["method"] == "ssh":
+                    repos.append(f"git@github.com:{repo.full_name}.git as {path}")
+                elif v["method"] == "https":
+                    repos.append(f"https://github.com/{repo.full_name}.git as {path}")
+                else:
+                    raise ValueError(v["method"])
+        else:
+            print(f"Unsupported autofetch: {k}", file=sys.stderr)
+            sys.exit(2)
+    return repos
+
+
+def main():
+    with open("ethclone.yaml", "r") as f:
+        try:
+            y = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print(str(exc), file=sys.stderr)
+            sys.exit(1)
+
+    repos = []
+    if "autofetch" in y:
+        repos += handle_autofetch(y["autofetch"])
+    if "other" in y:
+        repos += y["other"]
+    for repo in repos:
+        clone_repo(repo)
---
+++ ethclone/gitcmds.py
@@ -0,0 +1,121 @@
+import git
+from rich import progress
+from pathlib import Path
+import os
+from dataclasses import dataclass
+from threading import Thread
+
+
+class GitRichProgress:
+    max_name_length = 20
+
+    def __init__(self):
+        super().__init__()
+
+        self.progressbar = progress.Progress(
+            progress.SpinnerColumn(),
+            progress.TextColumn("{task.description}"),
+            progress.BarColumn(),
+            progress.TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+            progress.TimeRemainingColumn(),
+            progress.TextColumn("{task.fields[message]}"),
+        )
+        self.progressbar = self.progressbar.__enter__()
+
+    def __del__(self) -> None:
+        try:
+            self.progressbar.__exit__(None, None, None)
+        except:
+            pass
+
+    def task(self, name: str):
+        progressbar = self.progressbar
+
+        if len(name) > GitRichProgress.max_name_length - 3:
+            name = f"...{name[(-1 * (GitRichProgress.max_name_length-3)):]}"
+        name_format = "{0: <%s}" % GitRichProgress.max_name_length
+        name = name_format.format(name)
+
+        task = progressbar.add_task(
+            description=name,
+            total=100.0,
+            message="",
+        )
+
+        class RemoteProgress(git.RemoteProgress):
+            def update(
+                self,
+                op_code: int,
+                cur_count: str | float,
+                max_count: float | str | None = None,
+                message: str | None = "",
+            ) -> None:
+                progressbar.update(
+                    task_id=task,
+                    completed=cur_count,
+                    total=max_count,
+                    message=message,
+                )
+
+            def stop(self):
+                progressbar.stop_task(task)
+                progressbar.remove_task(task)
+
+        return RemoteProgress()
+
+
+@dataclass
+class CloneProcess:
+    url: str
+    dest: str
+    branch: str | None = None
+
+
+def _clonefunc(progress, repo, result):
+    task = progress.task(repo.dest)
+    try:
+        git.Repo.clone_from(
+            url=repo.url,
+            to_path=Path(repo.dest).resolve(),
+            progress=task,
+            branch=repo.branch,
+        )
+    except Exception as e:
+        task.stop()
+        result[repo.dest] = None
+        result[repo.dest] = e
+
+
+class GitCloneException(Exception):
+    def __init__(self, exceptions: list[Exception]):
+        self.exceptions = exceptions
+
+    def __str__(self):
+        return os.linesep.join([str(e) for e in self.exceptions if e is not None])
+
+
+def clone(repos: list[CloneProcess]) -> None:
+    progress = GitRichProgress()
+    threads = []
+    result = {}
+    for repo in repos:
+        threads.append(Thread(target=_clonefunc, args=(progress, repo, result)))
+        threads[-1].start()
+
+    for thread in threads:
+        thread.join()
+    del progress
+
+    for _, e in result.items():
+        if e is not None:
+            raise GitCloneException(result.values())
+
+
+if __name__ == "__main__":
+    c = CloneProcess(
+        url="https://github.com/Homebrew/brew", dest="TEST/1", branch="master"
+    )
+    c2 = CloneProcess(
+        url="https://github.com/Homebrew/brew", dest="TEST/2", branch="master"
+    )
+    clone([c, c2])
