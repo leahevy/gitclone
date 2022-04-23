d0cde2999d7b5d441fc298698c00e82373376c81
Leah Lackner <leah.lackner+github@gmail.com>
2022-04-23 12:13:52 +0200

    Better path checks in test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,8 +14,10 @@ from contextlib import contextmanager
 def cwd(path):
     oldpwd = os.getcwd()
     os.chdir(path)
-    assert os.getcwd() != ("/")
     try:
+        _, tail_path = os.path.splitdrive(os.path.normpath(os.getcwd()))
+        separated_path = [p for p in tail_path.split(os.path.sep) if p]
+        assert len(separated_path) > 1
         yield
     finally:
         os.chdir(oldpwd)
