5e2d168c3bd1f7839539242beeb676d15d8fb31e
Leah Lackner <leah.lackner+github@gmail.com>
2022-04-30 01:42:34 +0200

    Test main() -> test coverage 80%

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,11 @@
 import os
 import os.path
+import sys
 
+import pytest
 from typer.testing import CliRunner
 
-from gitclone.cli import cli
+from gitclone.cli import cli, main
 
 from .utils import coreconfig, write
 
@@ -50,6 +52,26 @@ def test_cli_clone_and_pull() -> None:
         )
         result = runner.invoke(cli, ["pull"])
         assert result.exit_code != 0  # TODO: Not implemented
+
+        assert os.path.exists(os.path.join("gitclone", ".git"))
+        assert os.path.exists(os.path.join("gitclone2", ".git"))
+
+
+def test_cli_main_clone() -> None:
+    with coreconfig() as f:
+        write(
+            f,
+            """
+            repositories:
+                - https://github.com/evyli/gitclone.git gitclone
+                - https://github.com/evyli/gitclone.git gitclone2
+            """,
+        )
+        sys.argv = ["gitclone"]
+        with pytest.raises(SystemExit) as e:
+            main()
+        assert e.type == SystemExit
+        assert e.value.code == 0
 
         assert os.path.exists(os.path.join("gitclone", ".git"))
         assert os.path.exists(os.path.join("gitclone2", ".git"))
